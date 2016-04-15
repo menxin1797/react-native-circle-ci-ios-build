84a03c592167804741a6befcad2398e84ab46395
Konstantin Raev <bestander@fb.com>
2016-04-15 11:30:09 +0100

    testing second repo sync with RN

---
+++ report-react-native-status.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# relies on $LINUX_REPO and $LINUX_SHA1 being set
+
+TEST_FAILURE_FILE="$LINUX_REPO/test-failure.txt"
+GITHUB_STATUS_URL="https://api.github.com/repos/facebook/react-native/statuses/$LINUX_SHA1?access_token=$GITHUB_TOKEN"
+
+TEST_STATUS="success"
+TEST_DESCRIPTION="OS X tests succeeded"
+
+if [[ -f $TEST_FAILURE_FILE ]]
+  then
+    TEST_STATUS="failure"
+    TEST_DESCRIPTION="OS X tests failed"
+fi
+
+curl \
+  --header "Content-Type: application/json" \
+  --data "{ \"state\": \"$TEST_STATUS\", \
+           \"target_url\": \"$CIRCLE_BUILD_URL\", \
+           \"description\": \"$TEST_DESCRIPTION\", \
+           \"context\": \"ci/circleci-os-x\" }" \
+  --request POST "$GITHUB_STATUS_URL"
